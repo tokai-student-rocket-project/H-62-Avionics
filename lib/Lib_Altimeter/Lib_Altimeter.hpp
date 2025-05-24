627d8da3df5331449c396dc4897dfd4871df798a
HirokiTsutsumi <hiroki1815@outlook.jp>
2025-05-24 16:06:31 +0900

    高度計のライブラリを作成中

---
+++ lib/Lib_Altimeter/Lib_Altimeter.hpp
@@ -0,0 +1,30 @@
+#pragma once
+
+#include <Arduino.h>
+#include <Wire.h>
+#include <Adafruit_LPS28.h>
+
+
+
+class Altimeter {
+public:
+    void initialize(uint32_t i2cAddress);
+    void getConfiguration();
+
+    float getPressure();
+    float getTemperature();
+
+    void setAutoReferencePressure(bool enable);
+    void setReferencePressure(float referencePressure_kPa);
+    void resetAutoReferencePressure();
+    void setAutozero(bool enable);
+    void resetAutozero();
+    float getAltitude(float temperature_C);
+    int16_t getReferencePressure();
+
+private:
+    Adafruit_LPS28* _lps28;
+
+    float _pressure_kPa;
+    int16_t _referencePressure_kPa;
+};
