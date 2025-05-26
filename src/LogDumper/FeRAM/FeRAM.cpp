4732a88abfc3800041a414fa95508d302ba6bbc3
HirokiTsutsumi <hiroki1815@outlook.jp>
2025-05-27 06:50:33 +0900

    FeRAMのテスト用ログダンパーを追加

---
+++ src/LogDumper/FeRAM/FeRAM.cpp
@@ -0,0 +1,91 @@
+#include <MsgPacketizer.h>
+#include "Lib_FRAM.hpp"
+
+const uint32_t FRAM_CS_PIN0 = 7;
+const uint32_t FRAM_CS_PIN1 = A1;
+const uint32_t FRAM_CS_PIN2 = A2;
+
+FRAM fram0(FRAM_CS_PIN0);
+FRAM fram1(FRAM_CS_PIN1);
+FRAM fram2(FRAM_CS_PIN2);
+
+// const uint8_t redled = 17; //GPIO17
+// const uint8_t greenled = 16; //GPIO16
+// const uint8_t blueled = 25; //GPIO25
+
+
+void dump(FRAM *fram)
+{
+  uint8_t data[4096];
+  uint32_t size = 0;
+  uint32_t writeAddress = 0;
+
+  for (uint32_t address = 0; address < FRAM::LENGTH; address++)
+  {
+    data[writeAddress] = fram->read(address);
+    size = writeAddress + 1;
+
+    if (data[writeAddress] == 0x00)
+    {
+      writeAddress = 0;
+
+      if (data[1] == 0x0A)
+      {
+        MsgPacketizer::feed(data, size);
+      }
+    }
+    else
+    {
+      writeAddress++;
+    }
+  }
+}
+
+void printHeader()
+{
+  Serial.print("millis");
+  Serial.print(",");
+  Serial.print("loggerUsage");
+  Serial.print(",");
+  Serial.print("framNumber");
+  Serial.print("\n");
+}
+
+void setup()
+{
+  Serial.begin(115200);
+  SPI.begin();
+
+  MsgPacketizer::subscribe_manual(0x0A,
+                                  [&](uint32_t millis, float loggerUsage, uint8_t framNumber)
+                                  {
+                                    Serial.print(millis);
+                                    Serial.print(",");
+                                    Serial.print(loggerUsage);
+                                    Serial.print(",");
+                                    Serial.print(framNumber);
+                                    Serial.print("\n");
+                                  });
+
+  // pinMode(redled, OUTPUT);
+  // pinMode(greenled, OUTPUT); 
+  // pinMode(blueled, OUTPUT);
+  // digitalWrite(blueled, LOW);
+
+  pinMode(LED_BUILTIN, OUTPUT);
+  digitalWrite(LED_BUILTIN, LOW); // 内蔵LEDを初期状態で消灯
+
+  while (!Serial);
+  delay(5000);
+  printHeader();
+
+  dump(&fram0);
+  dump(&fram1);
+  dump(&fram2);
+
+  digitalWrite(LED_BUILTIN, HIGH); // 内蔵LEDを点灯
+}
+
+void loop()
+{
+}
