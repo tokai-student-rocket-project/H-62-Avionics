a378f42cd5dc2244b383445dda300fc52299e4fb
HirokiTsutsumi <hiroki1815@outlook.jp>
2025-06-01 12:25:58 +0900

    Sensing Module 関係の開発を開始

---
+++ src/Flight/SensingModule/SensingModule.cpp
@@ -0,0 +1,323 @@
+#include <Arduino.h>
+#include <SPI.h>
+#include <Wire.h>
+#include <TaskManager.h>
+#include <MsgPacketizer.h>
+#include <movingAvg.h>
+#include <Calculus.h>
+#include <Filters.h>
+#include "Lib_Var.hpp"
+#include "Lib_CAN.hpp"
+#include "Lib_Telemeter.hpp"
+#include "Lib_BNO055.hpp"
+#include "Lib_Logger3.hpp"
+#include "Lib_Altimeter.hpp"
+#include "Lib_Thermistor.hpp"
+#include "Lib_PowerMonitor.hpp"
+#include "Lib_RateMonitor.hpp"
+#include "Lib_OutputPin.hpp"
+
+char ident = '\0';
+bool doLogging = false;
+
+CAN can(7);
+Telemeter telemeter;
+Logger logger(A1, A2, 2);
+
+OutputPin ledWork(LED_BUILTIN);
+OutputPin ledCanRx(0);
+OutputPin ledCanTx(1);
+OutputPin ledLoRaRx(4);
+OutputPin ledLoRaTx(5);
+
+uint8_t flightMode;
+uint16_t flightTime;
+RateMonitor monitor10Hz;
+RateMonitor monitor100Hz;
+
+// Altimeter altimeter;
+BNO055 bno055;
+PowerMonitor powerMonitor;
+Thermistor regulator1(A3);
+Thermistor regulator2(A4);
+Thermistor regulator3(A5);
+Thermistor conduction(A6);
+Thermistor outside(A0);
+
+float accelerationX_mps2, accelerationY_mps2, accelerationZ_mps2;
+float gyroscopeX_dps, gyroscopeY_dps, gyroscopeZ_dps;
+float roll_deg, pitch_deg, yaw_deg;
+float magnetometerX_nT, magnetometerY_nT, magnetometerZ_nT;
+
+float groundVoltage_V, batteryVoltage_V, tieVoltage_V, busVoltage_V;
+float groundCurrent_mA, batteryCurrent_mA, tieCurrent_mA, busCurrent_mA;
+float groundPower_mW, batteryPower_mW, tiePower_mW, busPower_mW;
+
+float temperatureRegulator1_degC, temperatureRegulator2_degC, temperatureRegulator3_degC, temperatureConduction_degC;
+float temperatureOutside_degC, temperatureInside_degC;
+
+float temperatureVentPort_degC, temperatureTankAtmosphere_degC;
+// uint32_t sutegomaTime_ms;
+// float sutegomeTaskRate_Hz;
+
+float pressure_kPa;
+float altitude_m;
+movingAvg altitudeAverage(10);
+Calculus::Differential<float> altitudeGradient(5);
+Calculus::Differential<float> verticalSpeedGradient(0.5);
+float verticalSpeed_mps;
+float verticalAcceleration_msp2;
+float estimated;
+float apogee;
+bool isFalling;
+
+Calculus::Integral<float> gyroscopeIntegralX;
+Calculus::Integral<float> gyroscopeIntegralY;
+Calculus::Integral<float> gyroscopeIntegralZ;
+
+Filter::HPF<float> accelerationHighPassX(5);
+Filter::HPF<float> accelerationHighPassY(5);
+Filter::HPF<float> accelerationHighPassZ(5);
+
+constexpr float gravity_mps2 = 9.80665;
+constexpr float initialPitch_deg = 90.0;
+constexpr float characteristicMass_kg = 12.41;
+float gravityX_mps2, gravityY_mps2, gravityZ_mps2;
+float linearAccelerationX_mps2, linearAccelerationY_mps2, linearAccelerationZ_mps2;
+float forceX_N;
+Calculus::Differential<float> linearAccelerationGradient(0.25);
+float jerkX_mps3;
+
+void task100Hz()
+{
+    ledWork.toggle();
+
+    float deltaTime = 1.0 / monitor100Hz.updateRate();
+
+    bno055.getAcceleration(&accelerationX_mps2, &accelerationY_mps2, &accelerationZ_mps2);
+    bno055.getGyroscope(&gyroscopeX_dps, &gyroscopeY_dps, &gyroscopeZ_dps);
+
+    roll_deg = gyroscopeIntegralX.get(gyroscopeX_dps, deltaTime);
+    pitch_deg = gyroscopeIntegralY.get(gyroscopeY_dps, deltaTime) + initialPitch_deg;
+    yaw_deg = gyroscopeIntegralZ.get(gyroscopeZ_dps, deltaTime);
+
+    gravityX_mps2 = gravity_mps2 * sin(radians(pitch_deg)) * cos(radians(yaw_deg));
+    gravityY_mps2 = gravity_mps2 * sin(radians(yaw_deg)) * cos(radians(roll_deg));
+    gravityZ_mps2 = gravity_mps2 * cos(radians(pitch_deg)) * cos(radians(roll_deg));
+
+    linearAccelerationX_mps2 = accelerationHighPassX.get(accelerationX_mps2 - gravityX_mps2, deltaTime);
+    linearAccelerationY_mps2 = accelerationHighPassY.get(accelerationY_mps2 - gravityY_mps2, deltaTime);
+    linearAccelerationZ_mps2 = accelerationHighPassZ.get(accelerationZ_mps2 - gravityZ_mps2, deltaTime);
+
+    const auto &logPacket = MsgPacketizer::encode(0x0A,
+                                                  ident, millis(), flightTime, flightMode, logger.getUsage(),
+                                                  accelerationX_mps2, accelerationY_mps2, accelerationZ_mps2,
+                                                  gyroscopeX_dps, gyroscopeY_dps, gyroscopeZ_dps,
+                                                  magnetometerX_nT, magnetometerY_nT, magnetometerZ_nT,
+                                                  roll_deg, pitch_deg, yaw_deg,
+                                                  forceX_N, jerkX_mps3,
+                                                  pressure_kPa, altitude_m, verticalSpeed_mps, verticalAcceleration_msp2, estimated, apogee, isFalling,
+                                                  groundVoltage_V, batteryVoltage_V, tieVoltage_V, busVoltage_V,
+                                                  groundCurrent_mA, batteryCurrent_mA, tieCurrent_mA, busCurrent_mA,
+                                                  groundPower_mW, batteryPower_mW, tiePower_mW, busPower_mW,
+                                                  temperatureRegulator1_degC, temperatureRegulator2_degC, temperatureRegulator3_degC, temperatureConduction_degC,
+                                                  temperatureOutside_degC, temperatureInside_degC,
+                                                  temperatureVentPort_degC, temperatureTankAtmosphere_degC);
+
+    if (doLogging)
+    {
+        logger.write(logPacket.data.data(), logPacket.data.size());
+    }
+}
+
+void task50Hz()
+{
+    ledWork.toggle();
+
+    temperatureOutside_degC = outside.getTemperature_degC();
+    // pressure_kPa = altimeter.getPressure();
+    // altitude_m = altimeter.getAltitude(temperatureOutside_degC + 273.15);
+
+    /*
+    ////////////////////////// Teleplot出力
+    Serial.print(">pressure:");
+    Serial.println(pressure_kPa);
+    Serial.print(">alti:");
+    Serial.println(altitude_m);
+    //////////////////////////
+    */
+}
+
+void task20Hz()
+{
+    ledWork.toggle();
+
+    bno055.getMagnetometer(&magnetometerX_nT, &magnetometerY_nT, &magnetometerZ_nT);
+}
+
+void task10Hz()
+{
+    ledWork.toggle();
+
+    float deltaTime = 1.0 / monitor10Hz.updateRate();
+
+    forceX_N = characteristicMass_kg * linearAccelerationX_mps2;
+    jerkX_mps3 = linearAccelerationGradient.get(linearAccelerationX_mps2, deltaTime);
+
+    can.sendDynamics(forceX_N, jerkX_mps3);
+
+    verticalSpeed_mps = altitudeGradient.get((float)altitudeAverage.reading((int16_t)(altitude_m * 10)) / 10.0, deltaTime);
+    verticalAcceleration_msp2 = verticalSpeedGradient.get(verticalSpeed_mps, deltaTime);
+
+    estimated = -verticalSpeed_mps / verticalAcceleration_msp2;
+    apogee = altitude_m + (verticalSpeed_mps * estimated + 0.5 * verticalAcceleration_msp2 * estimated * estimated);
+    isFalling = verticalSpeed_mps < 0;
+
+    can.sendTrajectory(isFalling, altitude_m);
+
+    ledCanTx.toggle();
+}
+
+void task5Hz()
+{
+    ledWork.toggle();
+
+    powerMonitor.getVoltage(&groundVoltage_V, &batteryVoltage_V, &tieVoltage_V, &busVoltage_V);
+    powerMonitor.getCurrent(&groundCurrent_mA, &batteryCurrent_mA, &tieCurrent_mA, &busCurrent_mA);
+    powerMonitor.getPower(&groundPower_mW, &batteryPower_mW, &tiePower_mW, &busPower_mW);
+    temperatureRegulator1_degC = regulator1.getTemperature_degC();
+    temperatureRegulator2_degC = regulator2.getTemperature_degC();
+    temperatureRegulator3_degC = regulator3.getTemperature_degC();
+    temperatureConduction_degC = conduction.getTemperature_degC();
+    // temperatureInside_degC = altimeter.getTemperature();
+}
+
+void task2Hz()
+{
+    const auto &airTelemetryPacket = MsgPacketizer::encode(0x0A,
+                                                           static_cast<uint32_t>(millis()),
+                                                           static_cast<char>(ident),
+                                                           static_cast<uint8_t>(logger.getUsage()),
+                                                           static_cast<bool>(doLogging),
+                                                           static_cast<uint8_t>(logger.framNumber()),
+                                                           static_cast<int16_t>(accelerationX_mps2 * 10),
+                                                           static_cast<int16_t>(accelerationY_mps2 * 10),
+                                                           static_cast<int16_t>(accelerationZ_mps2 * 10),
+                                                           static_cast<int16_t>(sqrt(accelerationX_mps2 * accelerationX_mps2 + accelerationY_mps2 * accelerationY_mps2 + accelerationZ_mps2 * accelerationZ_mps2) * 10),
+                                                           static_cast<int16_t>(roll_deg * 10),
+                                                           static_cast<int16_t>(pitch_deg * 10),
+                                                           static_cast<int16_t>(yaw_deg * 10),
+                                                           static_cast<int16_t>(forceX_N * 10),
+                                                           static_cast<int16_t>(jerkX_mps3 * 10),
+                                                           static_cast<int16_t>(altitude_m * 10),
+                                                           static_cast<int16_t>(verticalSpeed_mps * 10),
+                                                           static_cast<int16_t>(estimated * 10),
+                                                           static_cast<int16_t>(apogee * 10),
+                                                           static_cast<uint8_t>(groundVoltage_V * 10),
+                                                           static_cast<uint8_t>(batteryVoltage_V * 10),
+                                                           static_cast<uint8_t>(tieVoltage_V * 10),
+                                                           static_cast<uint8_t>(busVoltage_V * 10),
+                                                           static_cast<int16_t>(groundCurrent_mA * 10),
+                                                           static_cast<int16_t>(batteryCurrent_mA * 10),
+                                                           static_cast<int16_t>(tieCurrent_mA * 10),
+                                                           static_cast<int16_t>(busCurrent_mA * 10),
+                                                           static_cast<int8_t>(groundPower_mW / 100),
+                                                           static_cast<int8_t>(batteryPower_mW / 100),
+                                                           static_cast<int8_t>(tiePower_mW / 100),
+                                                           static_cast<int8_t>(busPower_mW / 100),
+                                                           static_cast<int16_t>(temperatureRegulator1_degC * 10),
+                                                           static_cast<int16_t>(temperatureRegulator2_degC * 10),
+                                                           static_cast<int16_t>(temperatureRegulator3_degC * 10),
+                                                           static_cast<int16_t>(temperatureConduction_degC * 10),
+                                                           static_cast<int16_t>(temperatureOutside_degC * 10),
+                                                           static_cast<int16_t>(temperatureInside_degC * 10),
+                                                           static_cast<int16_t>(temperatureVentPort_degC * 10),
+                                                           static_cast<int16_t>(temperatureTankAtmosphere_degC * 10));
+
+    telemeter.reserveData(airTelemetryPacket.data.data(), airTelemetryPacket.data.size());
+    telemeter.sendReservedData();
+    ledLoRaTx.toggle();
+}
+
+void setup()
+{
+    analogReadResolution(12);
+    Serial.begin(115200);
+    Wire.begin();
+    SPI.begin();
+
+    can.begin();
+    telemeter.initialize(924.2E6, 500E3);
+
+    powerMonitor.initialize();
+
+    bno055.begin();
+    // altimeter.initialize();
+    // altimeter.setReferencePressure();
+
+    altitudeAverage.begin();
+
+    Tasks.add(&task100Hz)->startFps(100);
+    Tasks.add(&task50Hz)->startFps(50);
+    Tasks.add(&task20Hz)->startFps(20);
+    Tasks.add(&task10Hz)->startFps(10);
+    Tasks.add(&task5Hz)->startFps(5);
+    Tasks.add(&task2Hz)->startFps(2);
+}
+
+void loop()
+{
+    Tasks.update();
+
+    if (can.available())
+    {
+        switch (can.getLatestLabel())
+        {
+        case Var::Label::FLIGHT_DATA:
+        {
+            bool newDoLogging;
+            can.receiveFlight(&flightMode, &flightTime, &newDoLogging, &ident);
+            ledCanRx.toggle();
+
+            if (doLogging != newDoLogging)
+            {
+                doLogging = newDoLogging;
+
+                if (doLogging)
+                {
+                    logger.reset();
+                    // altimeter.setReferencePressure();
+                    gyroscopeIntegralX.reset();
+                    gyroscopeIntegralY.reset();
+                    gyroscopeIntegralZ.reset();
+                    accelerationHighPassX.reset();
+                    accelerationHighPassY.reset();
+                    accelerationHighPassZ.reset();
+                }
+            }
+
+            break;
+        }
+
+            /*
+            case Var::Label::SUTEGOMA_TEMPERATURE:
+            {
+              can.receiveSutegomaTemperature(&temperatureVentPort_degC, &temperatureTankAtmosphere_degC);
+              ledCanRx.toggle();
+
+              break;
+            }
+            */
+
+            /*
+            case Var::Label::SUTEGOMA_PERFORMANCE:
+            {
+              can.receiveSutegomaPerformance(&sutegomaTime_ms, &sutegomeTaskRate_Hz);
+              ledCanRx.toggle();
+
+              break;
+            }
+            */
+        }
+    }
+}
---
+++ src/LogDumper/SensingModule/SensingModule.cpp
@@ -0,0 +1,285 @@
+#include <MsgPacketizer.h>
+#include "Lib_FRAM.hpp"
+
+FRAM fram0(A1);
+FRAM fram1(A2);
+FRAM fram2(2);
+// FRAM fram3(3);
+
+void dump(FRAM *fram)
+{
+  uint8_t data[4096];
+  uint32_t size = 0;
+  uint32_t writeAddress = 0;
+
+  for (uint32_t address = 0; address < FRAM::LENGTH; address++)
+  {
+    data[writeAddress] = fram->read(address);
+    size = writeAddress + 1;
+
+    if (data[writeAddress] == 0x00)
+    {
+      writeAddress = 0;
+
+      if (data[1] == 0x0A)
+      {
+        MsgPacketizer::feed(data, size);
+      }
+    }
+    else
+    {
+      writeAddress++;
+    }
+  }
+}
+
+void printHeader()
+{
+  Serial.print(F("ident"));
+  Serial.print(F(","));
+  Serial.print(F("millis"));
+  Serial.print(F(","));
+  Serial.print(F("flightTime"));
+  Serial.print(F(","));
+  Serial.print(F("flightMode"));
+  Serial.print(F(","));
+  Serial.print(F("loggerUsage"));
+  Serial.print(F(","));
+
+  Serial.print(F("accelerationX_mps2"));
+  Serial.print(F(","));
+  Serial.print(F("accelerationY_mps2"));
+  Serial.print(F(","));
+  Serial.print(F("accelerationZ_mps2"));
+  Serial.print(F(","));
+
+  Serial.print(F("gyroscopeX_dps"));
+  Serial.print(F(","));
+  Serial.print(F("gyroscopeY_dps"));
+  Serial.print(F(","));
+  Serial.print(F("gyroscopeZ_dps"));
+  Serial.print(F(","));
+
+  Serial.print(F("magnetometerX_nT"));
+  Serial.print(F(","));
+  Serial.print(F("magnetometerY_nT"));
+  Serial.print(F(","));
+  Serial.print(F("magnetometerZ_nT"));
+  Serial.print(F(","));
+
+  Serial.print(F("roll_deg"));
+  Serial.print(F(","));
+  Serial.print(F("pitch_deg"));
+  Serial.print(F(","));
+  Serial.print(F("yaw_deg"));
+  Serial.print(F(","));
+
+  Serial.print(F("forceX_N"));
+  Serial.print(F(","));
+  Serial.print(F("jerkX_mps3"));
+  Serial.print(F(","));
+
+  Serial.print(F("pressure_kPa"));
+  Serial.print(F(","));
+  Serial.print(F("altitude_m"));
+  Serial.print(F(","));
+  Serial.print(F("verticalSpeed_mps"));
+  Serial.print(F(","));
+  Serial.print(F("verticalAcceleration_msp2"));
+  Serial.print(F(","));
+  Serial.print(F("estimated"));
+  Serial.print(F(","));
+  Serial.print(F("apogee"));
+  Serial.print(F(","));
+  Serial.print(F("isFalling"));
+  Serial.print(F(","));
+
+  Serial.print(F("groundVoltage_V"));
+  Serial.print(F(","));
+  Serial.print(F("batteryVoltage_V"));
+  Serial.print(F(","));
+  Serial.print(F("tieVoltage_V"));
+  Serial.print(F(","));
+  Serial.print(F("busVoltage_V"));
+  Serial.print(F(","));
+
+  Serial.print(F("groundCurrent_mA"));
+  Serial.print(F(","));
+  Serial.print(F("batteryCurrent_mA"));
+  Serial.print(F(","));
+  Serial.print(F("tieCurrent_mA"));
+  Serial.print(F(","));
+  Serial.print(F("busCurrent_mA"));
+  Serial.print(F(","));
+
+  Serial.print(F("groundPower_mW"));
+  Serial.print(F(","));
+  Serial.print(F("batteryPower_mW"));
+  Serial.print(F(","));
+  Serial.print(F("tiePower_mW"));
+  Serial.print(F(","));
+  Serial.print(F("busPower_mW"));
+  Serial.print(F(","));
+
+  Serial.print(F("temperatureRegulator1_degC"));
+  Serial.print(F(","));
+  Serial.print(F("temperatureRegulator2_degC"));
+  Serial.print(F(","));
+  Serial.print(F("temperatureRegulator3_degC"));
+  Serial.print(F(","));
+  Serial.print(F("temperatureConduction_degC"));
+  Serial.print(F(","));
+
+  Serial.print(F("temperatureOutside_degC"));
+  Serial.print(F(","));
+  Serial.print(F("temperatureInside_degC"));
+  Serial.print(F(","));
+
+  Serial.print(F("temperatureVentPort_degC"));
+  Serial.print(F(","));
+  Serial.print(F("temperatureTankAtmosphere_degC"));
+  Serial.print(F("\n"));
+}
+
+void setup()
+{
+  Serial.begin(115200);
+  SPI.begin();
+
+  MsgPacketizer::subscribe_manual(0x0A,
+                                  [&](char ident, uint32_t millis, uint16_t flightTime, uint8_t flightMode, float loggerUsage,
+                                      float accelerationX_mps2, float accelerationY_mps2, float accelerationZ_mps2,
+                                      float gyroscopeX_dps, float gyroscopeY_dps, float gyroscopeZ_dps,
+                                      float magnetometerX_nT, float magnetometerY_nT, float magnetometerZ_nT,
+                                      float roll_deg, float pitch_deg, float yaw_deg,
+                                      float forceX_N, float jerkX_mps3,
+                                      float pressure_kPa, float altitude_m, float verticalSpeed_mps, float verticalAcceleration_msp2, float estimated, float apogee, bool isFalling,
+                                      float groundVoltage_V, float batteryVoltage_V, float tieVoltage_V, float busVoltage_V,
+                                      float groundCurrent_mA, float batteryCurrent_mA, float tieCurrent_mA, float busCurrent_mA,
+                                      float groundPower_mW, float batteryPower_mW, float tiePower_mW, float busPower_mW,
+                                      float temperatureRegulator1_degC, float temperatureRegulator2_degC, float temperatureRegulator3_degC, float temperatureConduction_degC,
+                                      float temperatureOutside_degC, float temperatureInside_degC,
+                                      float temperatureVentPort_degC, float temperatureTankAtmosphere_degC)
+                                  {
+                                    Serial.print(ident);
+                                    Serial.print(F(","));
+                                    Serial.print(millis);
+                                    Serial.print(F(","));
+                                    Serial.print(flightTime);
+                                    Serial.print(F(","));
+                                    Serial.print(flightMode);
+                                    Serial.print(F(","));
+                                    Serial.print(loggerUsage);
+                                    Serial.print(F(","));
+
+                                    Serial.print(accelerationX_mps2);
+                                    Serial.print(F(","));
+                                    Serial.print(accelerationY_mps2);
+                                    Serial.print(F(","));
+                                    Serial.print(accelerationZ_mps2);
+                                    Serial.print(F(","));
+
+                                    Serial.print(gyroscopeX_dps);
+                                    Serial.print(F(","));
+                                    Serial.print(gyroscopeY_dps);
+                                    Serial.print(F(","));
+                                    Serial.print(gyroscopeZ_dps);
+                                    Serial.print(F(","));
+
+                                    Serial.print(magnetometerX_nT);
+                                    Serial.print(F(","));
+                                    Serial.print(magnetometerY_nT);
+                                    Serial.print(F(","));
+                                    Serial.print(magnetometerZ_nT);
+                                    Serial.print(F(","));
+
+                                    Serial.print(roll_deg);
+                                    Serial.print(F(","));
+                                    Serial.print(pitch_deg);
+                                    Serial.print(F(","));
+                                    Serial.print(yaw_deg);
+                                    Serial.print(F(","));
+
+                                    Serial.print(forceX_N);
+                                    Serial.print(F(","));
+                                    Serial.print(jerkX_mps3);
+                                    Serial.print(F(","));
+
+                                    Serial.print(pressure_kPa);
+                                    Serial.print(F(","));
+                                    Serial.print(altitude_m);
+                                    Serial.print(F(","));
+                                    Serial.print(verticalSpeed_mps);
+                                    Serial.print(F(","));
+                                    Serial.print(verticalAcceleration_msp2);
+                                    Serial.print(F(","));
+                                    Serial.print(estimated);
+                                    Serial.print(F(","));
+                                    Serial.print(apogee);
+                                    Serial.print(F(","));
+                                    Serial.print(isFalling);
+                                    Serial.print(F(","));
+
+                                    Serial.print(groundVoltage_V);
+                                    Serial.print(F(","));
+                                    Serial.print(batteryVoltage_V);
+                                    Serial.print(F(","));
+                                    Serial.print(tieVoltage_V);
+                                    Serial.print(F(","));
+                                    Serial.print(busVoltage_V);
+                                    Serial.print(F(","));
+
+                                    Serial.print(groundCurrent_mA);
+                                    Serial.print(F(","));
+                                    Serial.print(batteryCurrent_mA);
+                                    Serial.print(F(","));
+                                    Serial.print(tieCurrent_mA);
+                                    Serial.print(F(","));
+                                    Serial.print(busCurrent_mA);
+                                    Serial.print(F(","));
+
+                                    Serial.print(groundPower_mW);
+                                    Serial.print(F(","));
+                                    Serial.print(batteryPower_mW);
+                                    Serial.print(F(","));
+                                    Serial.print(tiePower_mW);
+                                    Serial.print(F(","));
+                                    Serial.print(busPower_mW);
+                                    Serial.print(F(","));
+
+                                    Serial.print(temperatureRegulator1_degC);
+                                    Serial.print(F(","));
+                                    Serial.print(temperatureRegulator2_degC);
+                                    Serial.print(F(","));
+                                    Serial.print(temperatureRegulator3_degC);
+                                    Serial.print(F(","));
+                                    Serial.print(temperatureConduction_degC);
+                                    Serial.print(F(","));
+
+                                    Serial.print(temperatureOutside_degC);
+                                    Serial.print(F(","));
+                                    Serial.print(temperatureInside_degC);
+                                    Serial.print(F(","));
+
+                                    Serial.print(temperatureVentPort_degC);
+                                    Serial.print(F(","));
+                                    Serial.print(temperatureTankAtmosphere_degC);
+                                    Serial.print(F("\n"));
+                                  });
+
+  while (!Serial)
+    ;
+  delay(5000);
+  printHeader();
+  pinMode(LED_BUILTIN, OUTPUT);
+  digitalWrite(LED_BUILTIN, HIGH);
+  dump(&fram0);
+  dump(&fram1);
+  dump(&fram2);
+  dump(&fram3);
+  digitalWrite(LED_BUILTIN, LOW);
+}
+
+void loop()
+{
+}
