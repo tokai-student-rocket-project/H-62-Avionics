6fe6eed86235c92caa9e622411a141c0c731d8f9
HirokiTsutsumi <hiroki1815@outlook.jp>
2025-06-05 17:32:27 +0900

    成形

--- lib/Lib_Logger1/Lib_Logger1.cpp
+++ lib/Lib_Logger1/Lib_Logger1.cpp
@@ -1,32 +1,34 @@
 #include "Lib_Logger1.hpp"
 
-
-Logger::Logger(uint32_t csFram0) {
+Logger::Logger(uint32_t csFram0)
+{
   _fram0 = new FRAM(csFram0);
 }
 
-
-void Logger::reset() {
+void Logger::reset()
+{
   _offset = 0;
 }
 
-
-void Logger::dump() {
+void Logger::dump()
+{
   _fram0->dump();
 }
 
-
-void Logger::clear() {
+void Logger::clear()
+{
   _fram0->setWriteEnable();
   _fram0->clear();
 }
 
-
-uint32_t Logger::write(const uint8_t* data, uint32_t size) {
-  if (_offset + size >= FRAM::LENGTH) {
+uint32_t Logger::write(const uint8_t *data, uint32_t size)
+{
+  if (_offset + size >= FRAM::LENGTH)
+  {
     return size;
   }
-  else {
+  else
+  {
     uint32_t writeAddress = _offset;
     _fram0->setWriteEnable();
     _fram0->write(writeAddress, data, size);
@@ -35,39 +37,42 @@ uint32_t Logger::write(const uint8_t* data, uint32_t size) {
   _offset += size;
 
   ////////////////////////////////////////////////////////////
-  // Serial.print("[");
-  // Serial.print(getUsage(), 0);
-  // Serial.print("% | ");
-  // Serial.print(_offset);
-  // Serial.print("b / ");
-  // Serial.print(FRAM::LENGTH * 4);
-  // Serial.print("b] ");
+  Serial.print("[");
+  Serial.print(getUsage(), 0);
+  Serial.print("% | ");
+  Serial.print(_offset);
+  Serial.print("b / ");
+  Serial.print(FRAM::LENGTH * 1);
+  Serial.print("b] ");
 
-  // for (uint32_t i = 0; i < size; i++) {
-  //   Serial.print(data[i], HEX);
-  //   Serial.print((data[i] == 0x00) ? "\n" : " ");
-  // }
+  for (uint32_t i = 0; i < size; i++)
+  {
+    Serial.print(data[i], HEX);
+    Serial.print((data[i] == 0x00) ? "\n" : " ");
+  }
   ////////////////////////////////////////////////////////////
 
   return size;
 }
 
-
-uint32_t Logger::getOffset() {
+uint32_t Logger::getOffset()
+{
   return _offset;
 }
 
-
-float Logger::getUsage() {
+float Logger::getUsage()
+{
   return ((float)_offset / (float)(FRAM::LENGTH * 1)) * 100.0;
 }
 
-
-uint8_t Logger::framNumber() {
-  if (_offset >= FRAM::LENGTH) {
+uint8_t Logger::framNumber()
+{
+  if (_offset >= FRAM::LENGTH)
+  {
     return -1;
   }
-  else {
+  else
+  {
     return 0;
   }
 }
