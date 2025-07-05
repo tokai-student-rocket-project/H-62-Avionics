a98f2a5e0631ab911ce1965ca898105976e713d7
Tsutsumi Hiroki <hiroki1815@outlook.jp>
2025-07-06 02:11:22 +0900

    PowerMonitorに対応

--- src/Flight/PowerModule/PowerModule.cpp
+++ src/Flight/PowerModule/PowerModule.cpp
@@ -1,134 +1,65 @@
 #include <Arduino.h>
-#include <Adafruit_INA228.h>
 #include <TaskManager.h>
+#include "Lib_CAN.hpp"
+#include "Lib_PowerMonitor.hpp"
+#include "Lib_Var.hpp"
+#include "Lib_OutputPin.hpp"
 
-// Adafruit_INA228 External = Adafruit_INA228();
-// Adafruit_INA228 Battery = Adafruit_INA228();
+char ident = '\0';
+bool doLogging = false;
+uint8_t flightMode = 0;
+uint16_t flightTime = 0;
 
-namespace external
+CAN can(26);
+PowerMonitor powerMonitor;
+
+OutputPin batteryEn(0);
+OutputPin ltc4353GroundEn(28);
+OutputPin ltc4353BatteryEn(29);
+
+float groundVoltage_V, batteryVoltage_V, busVoltage_V;
+float groundCurrent_mA, batteryCurrent_mA, busCurrent_mA;
+float groundPower_mW, batteryPower_mW, busPower_mW;
+float groundTemperature_C, batteryTemperature_C, busTemperature_C;
+
+void task5Hz()
 {
-    Adafruit_INA228 ina228 = Adafruit_INA228();
-    void init();
-    void readData();
-}
+    powerMonitor.getVoltage(&groundVoltage_V, &batteryVoltage_V, &busVoltage_V);
+    powerMonitor.getCurrent(&groundCurrent_mA, &batteryCurrent_mA, &busCurrent_mA);
+    powerMonitor.getPower(&groundPower_mW, &batteryPower_mW, &busPower_mW);
+    powerMonitor.getTemperature(&groundTemperature_C, &batteryTemperature_C, &busTemperature_C);
 
-namespace battery
-{
-    Adafruit_INA228 ina228 = Adafruit_INA228();
-    void init();
-    void readData();
-}
-
-void external::init()
-{
-    external::ina228.setShunt(0.15, 10.0);
-    external::ina228.setAveragingCount(INA228_COUNT_16);
-    external::ina228.setVoltageConversionTime(INA228_TIME_150_us);
-    external::ina228.setCurrentConversionTime(INA228_TIME_280_us);
-}
-
-void battery::init()
-{
-    battery::ina228.setShunt(0.15, 10.0);
-    battery::ina228.setAveragingCount(INA228_COUNT_16);
-    battery::ina228.setVoltageConversionTime(INA228_TIME_150_us);
-    battery::ina228.setCurrentConversionTime(INA228_TIME_280_us);
-}
-
-void external::readData()
-{
-    Serial.println("=== EXT ===");
-    Serial.print("Current: ");
-    Serial.print(external::ina228.getCurrent_mA());
-    Serial.println(" mA");
-
-    Serial.print("Bus Voltage: ");
-    Serial.print(external::ina228.getBusVoltage_V());
-    Serial.println(" V");
-
-    Serial.print("Shunt Voltage: ");
-    Serial.print(external::ina228.getShuntVoltage_mV());
-    Serial.println(" mV");
-
-    Serial.print("Power: ");
-    Serial.print(external::ina228.getPower_mW());
-    Serial.println(" mW");
-
-    Serial.print("Energy: ");
-    Serial.print(external::ina228.readEnergy());
-    Serial.println(" J");
-
-    Serial.print("Charge: ");
-    Serial.print(external::ina228.readCharge());
-    Serial.println(" C");
-
-    Serial.print("Temperature: ");
-    Serial.print(external::ina228.readDieTemp());
-    Serial.println(" *C");
-
-    Serial.println();
-}
-
-void battery::readData()
-{
-    Serial.println("=== BAT ===");
-    Serial.print("Current: ");
-    Serial.print(battery::ina228.getCurrent_mA());
-    Serial.println(" mA");
-
-    Serial.print("Bus Voltage: ");
-    Serial.print(battery::ina228.getBusVoltage_V());
-    Serial.println(" V");
-
-    Serial.print("Shunt Voltage: ");
-    Serial.print(battery::ina228.getShuntVoltage_mV());
-    Serial.println(" mV");
-
-    Serial.print("Power: ");
-    Serial.print(battery::ina228.getPower_mW());
-    Serial.println(" mW");
-
-    Serial.print("Energy: ");
-    Serial.print(battery::ina228.readEnergy());
-    Serial.println(" J");
-
-    Serial.print("Charge: ");
-    Serial.print(battery::ina228.readCharge());
-    Serial.println(" C");
-
-    Serial.print("Temperature: ");
-    Serial.print(battery::ina228.readDieTemp());
-    Serial.println(" *C");
-
-    Serial.println();
+    Serial.print(">Voltage_V ");
 }
 
 void setup()
 {
     Serial.begin(115200);
-    while (!Serial)
-        if (!external::ina228.begin())
-        {
-            Serial.println("Couldn't find External Monitor");
-            while (1)
-                ;
-        }
+    SPI.begin();
 
-    if (!battery::ina228.begin())
-    {
-        Serial.println("Couldn't find Battery Monitor");
-        while (1)
-            ;
-    }
+    batteryEn.low();
 
-    Serial.println("Found External Monitor!");
-    Serial.println("Found Battery Monitor!");
+    can.begin();
+    powerMonitor.initialize();
 
-    Tasks.add(&external::readData)->startFps(10);
-    Tasks.add(&battery::readData)->startFps(10);
+    Tasks.add(&task5Hz)->startFps(5);
 }
 
 void loop()
 {
     Tasks.update();
+
+    if (can.available())
+    {
+        switch (can.getLatestLabel())
+        {
+        case Var::Label::FLIGHT_DATA:
+        {
+            bool newDoLogging;
+            can.receiveFlight(&flightMode, &flightTime, &newDoLogging, &ident);
+
+            break;
+        }
+        }
+    }
 }
