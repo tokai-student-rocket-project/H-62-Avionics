58bf0653ecb143d0eb1ae312a6e5b17af7e44f35
HirokiTsutsumi <hiroki1815@outlook.jp>
2025-08-17 14:20:01 +0900

    識別用の機能を追加

--- src/Ground/CSV/SensingModule/SensingModule.cpp
+++ src/Ground/CSV/SensingModule/SensingModule.cpp
@@ -366,7 +366,14 @@ void setup()
     display.setCursor(0, 0);
     display.setTextSize(1);
     display.setTextColor(SSD1306_WHITE);
+    display.println("GROUND SENSING MODULE");
     display.println("System Start");
+    display.println();
+    display.println("--- System Start ---");
+    display.println("====================");
+    display.println("   H-62  Avionics   ");
+    display.println("     SUBARU 1.3     ");
+    display.println("====================");
     display.display();
     delay(1000);
 
